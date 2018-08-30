9227d336a16c667449d525332da713560a62876c
RytisDr <r.drazdauskas@gmail.com>
2018-08-30 16:48:23 +0200

    Changes name parts solution to backticks

--- name-parts/name-parts.js
+++ name-parts/name-parts.js
@@ -1,12 +1,45 @@
 "use strict";
 
-const fullName = "John John Florence";
+/*MY SOLUTION TO RECOGNIZE THE NAMES */
 
-const space1 = fullName.indexOf(" ");
-const space2 = fullName.indexOf(" ", space1 + 1);
-const firstName = fullName.substring(0, space1);
-const secondName = fullName.substring(space1, space2);
-const lastName = fullName.substring(space2);
-console.log(firstName);
-console.log(secondName);
-console.log(lastName);
+const fullName = "John John";
+const fullNameSplit = fullName.split(" ");
+console.log(`First name is: ${fullNameSplit[0]}`);
+if (fullNameSplit.length == 2) {
+  console.log(`Last name is: ${fullNameSplit[fullNameSplit.length - 1]}`);
+} else if (fullNameSplit.length == 3) {
+  console.log(
+    `First name is: ${fullNameSplit[0]}.\nSecond name is: ${
+      fullNameSplit[1]
+    }.\nLast name is: ${fullNameSplit[fullNameSplit.length - 1]}`
+  );
+} else if (fullNameSplit.length == 4) {
+  console.log(
+    `First name is: ${fullNameSplit[0]}.\nSecond name is: ${
+      fullNameSplit[1]
+    }.\nThird name is: ${fullNameSplit[2]}.\nLast name is: ${
+      fullNameSplit[fullNameSplit.length - 1]
+    }`
+  );
+}
+
+/* WORKING BUT LONG AND HARD TO READ */
+/* let message =
+  fullNameSplit.length == 2
+    ? " Last name is:" + fullNameSplit[fullNameSplit.length - 1]
+    : fullNameSplit.length == 3
+      ? " Second name is:" +
+        fullNameSplit[1] +
+        " Last name is:" +
+        fullNameSplit[fullNameSplit.length - 1]
+      : fullNameSplit.length == 4
+        ? " Second name is:" +
+          fullNameSplit[1] +
+          " Third name is:" +
+          fullNameSplit[2] +
+          " Last name is:" +
+          fullNameSplit[fullNameSplit.length - 1]
+        : " and you only have one name";
+
+console.log("First name is:" + fullNameSplit[0] + message);
+ */
