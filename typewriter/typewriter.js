ecc657ff98527a5635f697351987367138c986a7
RytisDr <r.drazdauskas@gmail.com>
2018-08-31 19:43:18 +0200

    Fixes sounds to play correctly not limited to any input value.

--- typewriter/typewriter.js
+++ typewriter/typewriter.js
@@ -11,13 +11,27 @@ btn.addEventListener("click", function() {
 });
 
 function loop() {
-  N = 0;
-  let loopInterval = setInterval(function type() {
+  N = 1;
+  let loopInterval = setInterval(function() {
     if (N <= inputText.length) {
       let newText = inputText.substring(0, N);
+      makeSound(newText, N);
       document.querySelector("#typewriter").textContent = newText;
       N++;
     }
     if (N > inputText.length) clearInterval(loopInterval);
-  }, 400);
+  }, 600);
+}
+function makeSound(newText, N) {
+  let character = newText.charAt(N - 1);
+  if (character == " ") {
+    document.querySelector("#typespace").play();
+  } else {
+    if (character == character.toUpperCase()) {
+      document.querySelector("#typekey1").play();
+    }
+    if (character == character.toLowerCase()) {
+      document.querySelector("#typekey2").play();
+    }
+  }
 }
