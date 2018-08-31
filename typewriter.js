69acfaac644ec47b7d036c08c28a9ddbfc77ac80
RytisDr <r.drazdauskas@gmail.com>
2018-08-31 15:45:18 +0200

    Creates input window to type out the value, creates a loop.

--- typewriter.js
+++ typewriter.js
@@ -1,2 +1,24 @@
 "use strict";
+let inputText;
+let N;
+let btn = document.querySelector("#typeBtn");
 
+btn.addEventListener("click", function() {
+  inputText = document.querySelector("#inputText").value;
+  if (inputText.length > 0) {
+    loop();
+  }
+});
+
+function loop() {
+  N = 0;
+  setInterval(function() {
+    if (N <= inputText.length) {
+      let newText = inputText.substring(0, N);
+      document.querySelector("#typewriter").textContent = newText;
+      N++;
+    }
+
+    console.log(N);
+  }, 400);
+}
