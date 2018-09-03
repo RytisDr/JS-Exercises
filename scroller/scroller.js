8ba3db7e60b5838f899c3fe8e489517a2990b8e5
RytisDr <r.drazdauskas@gmail.com>
2018-09-03 11:23:09 +0200

    Now scrolls.

--- scroller/scroller.js
+++ scroller/scroller.js
@@ -4,44 +4,39 @@ window.addEventListener("DOMContentLoaded", init);
 
 let text;
 let position;
+let array;
 
 function init() {
-    console.log("init");
+  // Get original text
+  text = document.querySelector("#scroller").textContent;
 
-    // Get original text
-    text = document.querySelector("#scroller").textContent;
+  // fix up all white-space, replace with single space
+  // from: https://stackoverflow.com/questions/1981349/regex-to-replace-multiple-spaces-with-a-single-space
+  text = text.replace(/\s\s+/g, " ");
 
-    // fix up all white-space, replace with single space
-    // from: https://stackoverflow.com/questions/1981349/regex-to-replace-multiple-spaces-with-a-single-space
-    text = text.replace(/\s\s+/g, ' ');
+  // clear the original text from HTML
+  document.querySelector("#scroller").textContent = "";
 
-    // clear the original text from HTML
-    document.querySelector("#scroller").textContent = "";
+  array = Array.from(text);
 
-    // start at position 0
-    position = 0;
-
-    // start the loop
-    setTimeout(loop, 100);
+  // start the loop
+  setTimeout(loop, 100);
 }
 
 function loop() {
-    console.log("loop");
+  position = 0;
+  shiftAndPush();
 
-    // create the new text as a piece of the old one - 40 characters long
-    const newText = text.substring(position, position+40);
+  let joinedArr = array.join("");
+  const newText = joinedArr.substring(position, position + 40);
 
-    // display the new text
-    document.querySelector("#scroller").textContent = newText;
+  // display the new text
+  document.querySelector("#scroller").textContent = newText;
 
-    // move to the next character
-    position++;
-
-    // when we reach the end, start from the beginning
-    if( position > text.length ) {
-        position = 0;
-    }
-
-    // loop the loop
-    setTimeout(loop, 100);
+  // loop the loop
+  setTimeout(loop, 100);
+}
+function shiftAndPush() {
+  let firstItem = array.shift();
+  array.push(firstItem);
 }
