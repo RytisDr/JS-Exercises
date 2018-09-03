5beaeddd7610332ad515a48fee651dc00e43ec67
RytisDr <r.drazdauskas@gmail.com>
2018-09-03 20:51:03 +0200

    Now creates and appends a bar properly, adds color to the bar depending on height.

--- barChart/script.js
+++ barChart/script.js
@@ -1,35 +1,39 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", init);
-let divs;
+let divs = document.querySelectorAll("div");
 let divArray;
-let randomHeightValue;
 let firstItem;
+let barHeight;
 function init() {
-  divs = null;
-  divArray = Array.from(document.querySelectorAll("div"));
-  setTimeout(loop, 100);
-}
+  divArray = Array.from(divs);
 
+  setTimeout(loop, 300);
+}
 function loop() {
-  let randomHeightValue = randomNumber(0, 100);
-
+  barHeight = randomNumber(0, 100);
   shiftAndPush();
-  firstItem.style.height = randomHeightValue + "px";
-  displayArray();
-  setTimeout(loop, 100);
-}
+  appendItem();
 
+  setTimeout(loop, 300);
+}
 function shiftAndPush() {
   firstItem = divArray.shift();
+  sizeAndColor();
   divArray.push(firstItem);
 }
-
+function sizeAndColor() {
+  firstItem.style.height = barHeight + "px";
+  if (barHeight < 33) {
+    firstItem.style.backgroundColor = "red";
+  } else if (barHeight > 33 && barHeight < 66) {
+    firstItem.style.backgroundColor = "yellow";
+  } else if (barHeight > 66 && barHeight < 100) {
+    firstItem.style.backgroundColor = "green";
+  }
+}
+function appendItem() {
+  document.querySelector("body").appendChild(firstItem);
+}
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
-}
-function displayArray() {
-  let joinedArray = divArray.join("");
-  let newArray = joinedArray.substring(0, 40);
-  divs = newArray;
-  console.log(newArray);
 }
