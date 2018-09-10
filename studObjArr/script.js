9972cd7b2b65c341ea9155909feee923774d4227
RytisDr <r.drazdauskas@gmail.com>
2018-09-10 14:01:24 +0200

    Adds additional filtering and sorting functions

--- studObjArr/script.js
+++ studObjArr/script.js
@@ -26,9 +26,12 @@ function createStudents() {
   //loop();
   //WITH FOR EACH LOOP
   names.forEach(pushToArrays);
-
   console.table(students);
-  // ... TODO: build this function ...
+  const gryffindorStudents = students.filter(inGryffindor);
+  console.table(students.sort(sortByLastName));
+
+  console.table(gryffindorStudents);
+  console.table(studentsInHouse(houses.Slytherin));
 }
 
 let nameObject = {
@@ -48,10 +51,10 @@ let nameObject = {
 function pushToArrays(name) {
   const newObject = Object.create(nameObject);
   newObject.splitName(name);
-
   const randomHouse = Object.keys(houses)[
     Math.floor(Math.random() * Object.keys(houses).length)
   ];
+
   newObject.house = randomHouse;
   students.push(newObject);
 }
@@ -64,3 +67,32 @@ function pushToArrays(name) {
   }
 }
  */
+function sortByLastName(a, b) {
+  if (a.lastName < b.lastName) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
+function inGryffindor(student) {
+  if (student.house == "Gryffindor") {
+    return true;
+  } else {
+    return false;
+  }
+}
+const gryffindorStudents = students.filter(inGryffindor);
+console.table(gryffindorStudents);
+///FUNCTION TO FILTER BY HOUSE
+function studentsInHouse(house) {
+  const houseStudents = students.filter(inHouse);
+  function inHouse(student) {
+    if (student.house === house) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return houseStudents;
+}
