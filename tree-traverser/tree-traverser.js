7876478bbe3ef0ffd97afa19ecdd111fc7c845d7
RytisDr <r.drazdauskas@gmail.com>
2018-09-11 20:08:09 +0200

    Creates second version of tree traverser

--- tree-traverser/tree-traverser.js
+++ tree-traverser/tree-traverser.js
@@ -1,9 +1,11 @@
 "use strict";
-window.addEventListener("DOMContentLoaded", log1);
-//Version1
+window.addEventListener("DOMContentLoaded", log2);
+//VERSION1
 function log1() {
   let documentArr = Array.from(document.all);
   documentArr.forEach(element => {
+    //let attr = Array.from(element.attributes);
+    //console.log(attr);
     console.log("element: " + element.nodeName);
     //    console.log(element.attributes);
     if (element.attributes.length > 0) {
@@ -13,6 +15,23 @@ function log1() {
             `${element.attributes[i].nodeValue}"`
         );
       }
+    }
+  });
+}
+//VERSION2
+function log2() {
+  let documentArr = Array.from(document.all);
+  documentArr.forEach(element => {
+    if (element.attributes.length > 0) {
+      for (let i = 0; i < element.attributes.length; i++) {
+        console.log(
+          `<${element.tagName.toLowerCase()} ${
+            element.attributes[i].nodeName
+          }="${element.attributes[i].nodeValue}">`
+        );
+      }
+    } else {
+      console.log("<" + element.tagName.toLowerCase() + ">");
     }
   });
 }
